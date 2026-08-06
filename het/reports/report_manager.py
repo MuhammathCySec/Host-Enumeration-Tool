5d2375c8309cb6b2a99e4637f94a66c8490a17c6
MuhammathCySec <muhammath.mk@gmail.com>
2026-08-06 19:02:52 +0530

    Update report_manager.py

--- het/reports/report_manager.py
+++ het/reports/report_manager.py
@@ -8,14 +8,33 @@ def create_report_folder():
         "%Y-%m-%d_%H-%M-%S"
     )
 
+
+    # Get HET project root
+    base_dir = os.path.dirname(
+        os.path.dirname(
+            os.path.dirname(
+                os.path.abspath(__file__)
+            )
+        )
+    )
+
+
+    reports_dir = os.path.join(
+        base_dir,
+        "reports"
+    )
+
+
     folder = os.path.join(
-        "reports",
+        reports_dir,
         timestamp
     )
+
 
     os.makedirs(
         folder,
         exist_ok=True
     )
+
 
     return folder
